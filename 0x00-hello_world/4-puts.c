a7e33dcfcaa9a8111266bdaa600e5e2aca8a43d7
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-20 10:49:14 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include stdio.h
+/*this prints out using puts*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
