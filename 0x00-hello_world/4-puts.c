e23e6ef4b82542029bf539ee5cb28e0eb72891ef
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-20 10:56:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#include stdio.h
-/*this prints out using puts*/
+#include <stdio.h>
+
+/*
+ * this is a comment about the main function
+ * prints out using the function puts
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
