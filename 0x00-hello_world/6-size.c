53411be47474d1a04baf5e9b77dce03c47856820
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-21 13:58:04 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry function
+ * Return: always 0 success
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("size of a char: %ld byte(s)\n", sizeof(a));
+printf("size of an int: %ld byte(s)\n", sizeof(b));
+printf("size of a long int: %ld byte(s)\n", sizeof(c));
+printf("size of a long long int: %ld byte(s)\n", sizeof(d));
+printf("size of a float: %ld byte(s)\n", sizeof(e));
+return (0);
+}
+
+
+
+
+
