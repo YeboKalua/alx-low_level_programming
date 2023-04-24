ddec7249b2feab0bbd2615f5a02a918e2621fc98
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-24 15:18:06 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+/**
+ * main - main function
+ * description: prints numbers with commas
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n = 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
