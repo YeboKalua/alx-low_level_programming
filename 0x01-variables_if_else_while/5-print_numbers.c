698b7a5be720a824d9f9c1206e05f4366c60d25d
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-24 14:30:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * description: print numbers from 0 to 9
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
