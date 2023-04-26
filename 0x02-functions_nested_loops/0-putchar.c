171819b54a9f6742e598a82bc1925b2f59b2467b
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-25 23:49:49 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry function
+ * description: prints out the word putchar
+ * Return: zero if successful
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
