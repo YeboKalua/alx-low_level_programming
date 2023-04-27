fcff63e37638cf7576198ae3eb67a4fd69c595c5
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-27 12:31:14 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,25 +10,21 @@
  *
  */
 
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
-
-	return (0);
 }
