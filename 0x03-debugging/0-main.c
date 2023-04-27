30c1207a123b2b8bc7049b0b9d04842a7cb91058
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-27 11:30:52 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry function
+ * description: test if code works on 0
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
