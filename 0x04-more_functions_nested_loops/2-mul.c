3c799b67bc61081fad726caa39657026761fdb3c
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-27 16:16:19 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - main function
+ * description: multiplies two integers
+ * @a: integer
+ * @b: integer
+ * Return: return int.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
