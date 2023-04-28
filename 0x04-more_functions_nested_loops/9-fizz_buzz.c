e5d5f840179ee8114e0d92117a08fcf765b95ff3
Yebo Kalua <kaluayebo2@gmail.com>
2023-04-27 18:48:58 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * description: fizz buzz test
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 15 == 0)
+		{
+			printf("FizzBuzz%s", " ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz%s", " ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz%s", " ");
+		}
+		else
+		{
+			printf("%d%s", n, " ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
