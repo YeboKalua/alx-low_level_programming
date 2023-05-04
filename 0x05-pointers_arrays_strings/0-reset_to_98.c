0b7a7155879662c086997f82cb67b727a1a30e20
Yebo Kalua <kaluayebo2@gmail.com>
2023-05-03 18:36:11 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - main function
+ * @*n : pointer to variable n
+ * @n : integer
+ * desription : changes value of n using pointer
+ * Returns : void 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
