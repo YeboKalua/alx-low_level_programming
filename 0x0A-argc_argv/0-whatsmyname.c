0e2134c9173b0078e35fccb60f208eda428527df
Yebo Kalua <kaluayebo2@gmail.com>
2023-05-12 03:22:38 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * description : prints the name of program
+ * @argv : command line input
+ * @argc : number of arguments
+ * Return: 0 if success
+ *
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
