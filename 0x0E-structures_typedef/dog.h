6ad17c2c4216573ba6fa41a5f840dd8326d35e57
Yebo Kalua <kaluayebo2@gmail.com>
2023-05-23 07:06:28 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#define
+/**
+ * struct dog - dog specs
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: describes dog in detail
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
