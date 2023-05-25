e9078ab37b7f797012331656877b27e4cca43751
Yebo Kalua <kaluayebo2@gmail.com>
2023-05-25 14:53:16 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,6 +13,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s;
 
+	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
@@ -24,7 +26,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	printf("\n");
 
 	va_end(args);
+
+	printf("\n");
 }
