c5d0ebe346d9f4740efc42af940f4ca50ee9d640
Yebo Kalua <kaluayebo2@gmail.com>
2023-05-27 07:13:09 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+#include <stdlb.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: int result
+ */
+int main(int argc, char **argv)
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_of_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - adds numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: result int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: result int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - subtracts numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: result int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - subtracts numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: result int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - subtracts numbers
+ * @a: integer a
+ * @b: integer b
+ * Return: result int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
