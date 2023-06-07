96ebe578b01541159dac7541b6f2ac797f4ba48c
Yebo Kalua <kaluayebo2@gmail.com>
2023-06-07 06:17:55 -0700

    correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (current != NULL)
 	{
-		if (n == idx)
+		if (i == idx)
 		{
 			toAdd->next = current->next;
 			current->next = toAdd;
