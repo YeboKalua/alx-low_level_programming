410250c7bdc14ba637a2b2cd2d7d171c7618551e
Yebo Kalua <kaluayebo2@gmail.com>
2023-06-08 20:05:59 -0700

    get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - checks size of machine
+ * Return: 0 for big 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
