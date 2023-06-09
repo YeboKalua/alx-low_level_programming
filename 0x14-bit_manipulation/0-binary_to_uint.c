85e0a15c46b2d8da687242a8077e0f97d76662a2
Yebo Kalua <kaluayebo2@gmail.com>
2023-06-08 19:12:15 -0700

    convert binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while (*b != '\0')
 	{
+		if (*b != '0' && *b != '1')
+			return (0);
 		n <<= 1;
 		if (*b == '1')
 			n |= 1;
